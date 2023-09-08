ce2c39f34dd54729b11321c5c6c396e7b2e70019
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-08 23:59:57 +0300

    PDFs With Python

--- pdf.py
+++ pdf.py
@@ -1,10 +1,11 @@
 import PyPDF2
 
-with open('twopage.pdf','rb') as file: # we use 'rb' because we want to read a binary. The pdf is in a format of binary
+with open('dummy.pdf','rb') as file: # we use 'rb' because we want to read a binary. The pdf is in a format of binary
 	reader = PyPDF2.PdfFileReader(file)
-	print(reader.numPages)
-	print(reader.getPage(0))
-	print(reader.getPage(1))
-	#print(reader.rotate(180)) # we will get an error. because the object 'reader' can not be rotated. we need to get the page to be rotated.
 	page = reader.getPage(0)
-	print(page.rotate(180))
+	page.rotateCounterClockwise(90)
+
+	writer = PyPDF2.PdfFileWriter()
+	writer.addPage(page)
+	with open('tilt.pdf','wb') as new_file:
+		writer.write(new_file)
